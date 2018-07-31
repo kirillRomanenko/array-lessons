fd5e40efee40e7ea892618e4c691b19d56a5a3b1
KirillRomanenko <kromanenko@oggettoweb.com>
2018-07-31 15:03:35 +0300

    create convolution array (task #1)

---
+++ convolution-array.js
@@ -0,0 +1,24 @@
+// 1) Найдите самого старшего.
+// 2) Посчитайте средний возраст.
+// 3) Сгруппируйте пользователей по возрасту.
+
+
+const users = [
+    { name: 'Vasya', age: 19, rate: 900 },
+    { name: 'Dima', age: 18, rate: 1000 },
+    { name: 'Sveta', age: 13, rate: 85 },
+    { name: 'Peyta', age: 14, rate: 100 },
+    { name: 'Seryozha', age: 18, rate: 900 },
+    { name: 'Valera', age: 14, rate: 150 }
+];
+
+console.log('Task #1\n');
+
+const userOld = users.reduce(function (prev, current) {
+    if(current.age > prev.age) {
+        return current;
+    }
+    return prev;
+});
+
+console.log(userOld);
